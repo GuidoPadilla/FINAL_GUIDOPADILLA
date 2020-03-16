4073c386ab8065df2d7f220919c1b05e6b615a00
GuidoPadilla <memona1234>
2020-03-15 21:43:42 -0600

    change

--- app/src/main/java/com/example/mygithub2/HomeFragment.kt
+++ app/src/main/java/com/example/mygithub2/HomeFragment.kt
@@ -8,18 +8,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.os.bundleOf
 import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
-import com.example.mygithub2.ApiService
-import com.example.mygithub2.ViewModelHome
 import com.example.mygithub2.databinding.FragmentHomeBinding
-import kotlinx.android.synthetic.main.fragment_home.*
 
 
-/**
- * A simple [Fragment] subclass.
- */
+
+
 class HomeFragment : Fragment() {
     private lateinit var api:ApiService
 
@@ -35,15 +29,12 @@ class HomeFragment : Fragment() {
 
         binding.setLifecycleOwner (this)
         binding.models=viewModel
-
-        //al presionar el boton de buscar
         binding.searchButton.setOnClickListener {
             viewModel.valor =binding.userGit.text.toString()
             viewModel.getuserGithubProperties()
 
         }
 
-        //el observer que decide si mostrar los demas botones dependiendo la respuesta
         viewModel.bandera.observe(viewLifecycleOwner, Observer { bandera ->
             bandera?.let {
                 viewModel.bandera.value = null
@@ -51,7 +42,7 @@ class HomeFragment : Fragment() {
                     binding.buttonRepos.visibility=View.VISIBLE
                     binding.imgs.visibility=View.VISIBLE
                 }else if(bandera==true){
-                    Toast.makeText(activity,"El usuario que ingreso no bandera",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(activity,"No existe",Toast.LENGTH_SHORT).show()
                     binding.buttonRepos.visibility=View.GONE
                     binding.imgs.visibility=View.GONE
                 }
--- app/src/main/java/com/example/mygithub2/ViewModelHome.kt
+++ app/src/main/java/com/example/mygithub2/ViewModelHome.kt
@@ -4,8 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import retrofit2.Call
-import com.example.mygithub2.ApiService
-import com.example.mygithub2.userGit
 import retrofit2.Response
 
 class ViewModelHome : ViewModel() {
@@ -20,43 +18,33 @@ class ViewModelHome : ViewModel() {
 
     val property: LiveData<userGit>
         get() = _property
-//In your network successfull response
 
-
-    // The external immutable LiveData for the response String
     val resp: LiveData<String>
         get() = _response
 
-    /**
-     * Call getMarsRealEstateProperties() on init so we can display status immediately.
-     */
+
     init {
 
     }
 
-    /**
-     * Sets the value of the response LiveData to the Mars API status or the successful number of
-     * Mars properties retrieved.
-     */
+
     fun getuserGithubProperties() {
         ApiServices.retrofitService.getProperties(valor).enqueue( object: retrofit2.Callback<userGit> {
             override fun onFailure(call: Call<userGit>, t: Throwable) {
                 _response.value = "Error " + t.message
             }
 
-            //una vez tiene la respuesta de la pagina
             override fun onResponse(call: Call<userGit>, response: Response<userGit>){
 
-                //muestra el nombre del usuario
-                _response.value = "Usuario: "+response.body()?.login
+                _response.value = "USER: "+response.body()?.login
                 if (response.body()?.login!=null){
-                    _response.value = "Usuario: "+response.body()?.login
+                    _response.value = "USER: "+response.body()?.login
                     _property.value=response.body()
                     bandera.value=false
 
 
-                }else{//de lo contrario deice que no lo encontro
-                    _response.value = "No bandera"
+                }else{
+                    _response.value = "No existe"
                     bandera.value = true
                 }
             }
